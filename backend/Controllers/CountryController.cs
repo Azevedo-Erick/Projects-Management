4c35db70364a20b35b9352a4757ef4d40ec50ee5
Azevedo-Erick <azevedoerick762@gmail.com>
2023-04-14 01:16:59 -0300

    feat: controller de country completo

--- backend/Controllers/CountryController.cs
+++ backend/Controllers/CountryController.cs
@@ -1,6 +1,13 @@
+using AspNetCore.IQueryable.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectsManagement.Data;
+using ProjectsManagement.Dtos;
+using ProjectsManagement.Dtos.Country;
+using ProjectsManagement.Factories;
 using ProjectsManagement.Models;
+using ProjectsManagement.QueryParams;
 
 namespace ProjectsManagement.Controllers;
 
@@ -9,27 +16,113 @@ namespace ProjectsManagement.Controllers;
 public class CountryController : ControllerBase
 {
 
+    ProjectsManagementContext Context;
+
+    CountryController(ProjectsManagementContext context)
+    {
+        Context = context;
+    }
+
     [AllowAnonymous]
     [HttpGet("/v1/countries")]
-    public async Task<IActionResult> Get() { return null; }
-
+    public async Task<IActionResult> Get([FromQuery] ContryQueryParams queryParams)
+    {
+        var data = await Context.Countries.AsQueryable().Apply(queryParams).ToListAsync();
+        return StatusCode(
+            200,
+            new BaseResponseDto<ResponseCountryDto>(
+                data.Select(
+                        x => CountryFactory.FromModelToDto(x)
+                    ).ToList()
+                )
+            );
+    }
 
     [HttpGet("/v1/countries/{id:int}")]
-    public async Task<IActionResult> GetById([FromRoute] int id) { return null; }
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        var data = await Context.Countries.Where(x => x.Id == id).FirstOrDefaultAsync();
+        if (data == null)
+        {
+            return StatusCode(
+            400);
+        }
+        return StatusCode(
+           200,
+           new BaseResponseDto<ResponseCountryDto>(
+                   CountryFactory.FromModelToDto(data)
+               )
+           );
+
+    }
 
 
 
     [HttpPost("/v1/countries")]
-    public async Task<IActionResult> Post(Country dto) { return null; }
+    public async Task<IActionResult> Post(CreateCountryDto dto)
+    {
+        if (!ModelState.IsValid) { }
+
+        var data = CountryFactory.FromDtoToModel(dto);
+
+        await Context.Countries.AddAsync(data);
+        Context.SaveChanges();
+
+        return StatusCode(
+           200,
+           new BaseResponseDto<ResponseCountryDto>(
+                   CountryFactory.FromModelToDto(data)
+               )
+           );
+    }
 
 
 
     [HttpPatch("/v1/countries/{id:int}")]
-    public async Task<IActionResult> Update([FromRoute] int id, [FromBody] Country dto) { return null; }
+    public async Task<IActionResult> Update([FromRoute] int id, [FromBody] CreateCountryDto dto)
+    {
+        if (!ModelState.IsValid) { }
+
+        var element = await Context.Countries.Where(x => x.Id == id).FirstOrDefaultAsync();
+
+        if (element == null)
+        {
+            return StatusCode(
+            400);
+        }
+
+        var data = CountryFactory.FromDtoToModel(dto);
+
+        element.Name = data.Name;
+        Context.Countries.Update(element);
+
+        Context.SaveChanges();
+
+        return StatusCode(
+           200,
+           new BaseResponseDto<ResponseCountryDto>(
+                   CountryFactory.FromModelToDto(element)
+               )
+           );
+    }
 
 
 
     [HttpDelete("/v1/countries/{id:int}")]
-    public async Task<IActionResult> Delete([FromRoute] int id) { return null; }
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        var element = await Context.Countries.Where(x => x.Id == id).FirstOrDefaultAsync();
+
+        if (element == null)
+        {
+            return StatusCode(
+            400);
+        }
+        Context.Countries.Remove(element);
+        return StatusCode(
+           200,
+           new BaseResponseDto<ResponseCountryDto>()
+           );
+    }
 
 }
