499fb64b012418598a4c5eca69e4c90a151536f8
Azevedo-Erick <azevedoerick762@gmail.com>
2023-04-14 01:14:05 -0300

    fix: ajustes no construtor de country

--- backend/Models/Country.cs
+++ backend/Models/Country.cs
@@ -2,6 +2,14 @@ namespace ProjectsManagement.Models;
 
 public class Country : BaseEntity
 {
+    public Country(string name)
+    {
+        Name = name;
+    }
+
+    public Country()
+    {
+    }
 
     public string Name { get; set; }
 }
