b639e422987b030dcfbc6de809b6c51b8eb4a740
Azevedo-Erick <azevedoerick762@gmail.com>
2023-04-15 00:43:20 -0300

    feat: adição de construtores sobrecarregados

--- backend/Models/ContactInfo.cs
+++ backend/Models/ContactInfo.cs
@@ -2,11 +2,34 @@ namespace ProjectsManagement.Models;
 
 public class ContactInfo : BaseEntity
 {
+    public ContactInfo()
+    {
+    }
 
-    public string Value { get; set; }
+    public ContactInfo(string value, ContactType type)
+    {
+        Value = value;
+        Type = type;
+    }
+
+    public ContactInfo(string value, int contactTypeId)
+    {
+        Value = value;
+        ContactTypeId = contactTypeId;
+    }
+
+    public ContactInfo(string value, ContactType type, int contactTypeId)
+    {
+        Value = value;
+        Type = type;
+        ContactTypeId = contactTypeId;
+    }
+
+
     public ContactType Type { get; set; }
 
-
+    public string Value { get; set; }
     public int ContactTypeId { get; set; }
+
 
 }
--- backend/Models/ContactType.cs
+++ backend/Models/ContactType.cs
@@ -2,6 +2,14 @@ namespace ProjectsManagement.Models;
 
 public class ContactType : BaseEntity
 {
+    public ContactType()
+    {
+    }
+
+    public ContactType(string name)
+    {
+        Name = name;
+    }
 
     public string Name { get; set; }
 }
