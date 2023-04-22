ea624fb8233df378d3adf42bec72f3ff66badee7
Azevedo-Erick <azevedoerick762@gmail.com>
2023-04-21 23:35:05 -0300

    feat: ajustes no recipient do responseNotificationDto

--- backend/Dtos/Notification/ResponseNotificationDto.cs
+++ backend/Dtos/Notification/ResponseNotificationDto.cs
@@ -1,3 +1,4 @@
+using ProjectsManagement.Dtos.Person;
 using ProjectsManagement.Models;
 
 namespace ProjectsManagement.Dtos.Notification;
@@ -7,7 +8,7 @@ public class ResponseNotificationDto
     public string Message { get; set; }
     public string Type { get; set; }
     public bool IsRead { get; set; }
-    public Person Recipient { get; set; }
+    public ResponsePersonDto Recipient { get; set; }
 
 
 }
