3ddeba4a0155f80e1dbc7e2ea9d504defb2385a5
Azevedo-Erick <azevedoerick762@gmail.com>
2023-04-21 23:42:55 -0300

    feat: crud de role parcialmente completo

--- backend/Controllers/RoleController.cs
+++ backend/Controllers/RoleController.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectsManagement.Data;
+using ProjectsManagement.Dtos;
+using ProjectsManagement.Dtos.Role;
+using ProjectsManagement.Mappers;
 using ProjectsManagement.Models;
 
 namespace ProjectsManagement.Controllers;
@@ -9,18 +14,45 @@ namespace ProjectsManagement.Controllers;
 public class RoleController : ControllerBase
 {
 
+    private readonly ProjectsManagementContext _context;
+
+    public RoleController(ProjectsManagementContext context)
+    {
+        _context = context;
+    }
+
     [AllowAnonymous]
     [HttpGet("/v1/roles")]
-    public async Task<IActionResult> Get() { return null; }
+    public async Task<IActionResult> Get()
+    {
+        var data = await _context.Roles.ToListAsync();
+        return StatusCode(200, new BaseResponseDto<ResponseRoleDto>(data.Select(RoleMapper.FromModelToDto).ToList()));
+
+    }
 
 
     [HttpGet("/v1/roles/{id:int}")]
-    public async Task<IActionResult> GetById([FromRoute] int id) { return null; }
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        var data = await _context.Roles.Where(x => x.Id == id).Include(x => x.Permissions).FirstOrDefaultAsync();
+        if (data == null)
+        {
+            return StatusCode(400, new BaseResponseDto<ResponseRoleDto>());
+        }
+        return StatusCode(200, new BaseResponseDto<ResponseRoleDto>(RoleMapper.FromModelToDto(data)));
+    }
 
 
 
     [HttpPost("/v1/roles")]
-    public async Task<IActionResult> Post(Role dto) { return null; }
+    public async Task<IActionResult> Post(CreateRoleDto dto)
+    {
+        if (!ModelState.IsValid) { }
+
+
+        return StatusCode(200, new BaseResponseDto<ResponseRoleDto>());
+
+    }
 
 
 
