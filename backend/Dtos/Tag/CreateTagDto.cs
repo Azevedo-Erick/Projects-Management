c6ea89fce0e8623a7a6bc4c07dc05d0cb9934333
Azevedo-Erick <azevedoerick762@gmail.com>
2023-04-21 23:33:15 -0300

    feat: base dos dto's de tag

---
+++ backend/Dtos/Tag/CreateTagDto.cs
@@ -0,0 +1,13 @@
+namespace ProjectsManagement.Dtos.Tag;
+
+public class CreateTagDto
+{
+    public string Title { get; set; }
+    public string HexColor { get; set; }
+    public CreateTagDto()
+    {
+        // constructor logic here
+    }
+
+    // class members here
+}
---
+++ backend/Dtos/Tag/ResponseTagDto.cs
@@ -0,0 +1,13 @@
+namespace ProjectsManagement.Dtos.Tag;
+
+public class ResponseTagDto
+{
+    public string Title { get; set; }
+    public string HexColor { get; set; }
+    public ResponseTagDto()
+    {
+        // constructor logic here
+    }
+
+    // class members here
+}
