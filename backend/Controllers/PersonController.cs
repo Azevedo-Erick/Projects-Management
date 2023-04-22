2ccd6d30d03516803f3513ecb43d81f68d6ef7ea
Azevedo-Erick <azevedoerick762@gmail.com>
2023-04-21 23:42:38 -0300

    feat: crud de person parcialmente completo

--- backend/Controllers/PersonController.cs
+++ backend/Controllers/PersonController.cs
@@ -1,6 +1,13 @@
+using AspNetCore.IQueryable.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectsManagement.Data;
+using ProjectsManagement.Dtos;
+using ProjectsManagement.Dtos.Person;
+using ProjectsManagement.Mappers;
 using ProjectsManagement.Models;
+using ProjectsManagement.QueryParams;
 
 namespace ProjectsManagement.Controllers;
 
@@ -9,18 +16,57 @@ namespace ProjectsManagement.Controllers;
 public class PersonController : ControllerBase
 {
 
+    private readonly ProjectsManagementContext _context;
+
+    public PersonController(ProjectsManagementContext context)
+    {
+        _context = context;
+    }
+
     [AllowAnonymous]
     [HttpGet("/v1/persons")]
-    public async Task<IActionResult> Get() { return null; }
+    public async Task<IActionResult> Get([FromQuery] PersonQueryParams queryParams)
+    {
+        var data = await _context.Persons.AsQueryable().Apply(queryParams).ToListAsync();
+        return StatusCode(200, new BaseResponseDto<ResponsePersonDto>(data.Select(PersonMapper.FromModelToDto).ToList()));
+    }
 
 
     [HttpGet("/v1/persons/{id:int}")]
-    public async Task<IActionResult> GetById([FromRoute] int id) { return null; }
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        var data = await _context.Persons.Where(x => x.Id == id).FirstOrDefaultAsync();
+        if (data == null)
+        {
+            return StatusCode(400, new BaseResponseDto<ResponsePersonDto>());
+        }
+        return StatusCode(200, new BaseResponseDto<ResponsePersonDto>(PersonMapper.FromModelToDto(data)));
+    }
 
 
 
     [HttpPost("/v1/persons")]
-    public async Task<IActionResult> Post(Person dto) { return null; }
+    public async Task<IActionResult> Post(CreatePersonDto dto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return StatusCode(400);
+        }
+
+        var data = PersonMapper.FromDtoToModel(dto);
+
+        var addresses = await _context.Addresses.Where(x => dto.Addresses.Contains(x.Id)).ToListAsync();
+        var contacts = await _context.ContactInfos.Where(x => dto.Contacts.Contains(x.Id)).ToListAsync();
+
+        data.Contacts = contacts;
+        data.Addresses = addresses;
+
+        _context.Persons.Add(data);
+        _context.SaveChanges();
+
+        return StatusCode(200, new BaseResponseDto<ResponsePersonDto>(PersonMapper.FromModelToDto(data)));
+
+    }
 
 
 
