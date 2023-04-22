a1bb8006ea09ccc8c64509dd7bc48ec0f7aeb80b
Azevedo-Erick <azevedoerick762@gmail.com>
2023-04-21 23:40:06 -0300

    ref: refatoração nos dto's

--- backend/Dtos/BaseResponseDto.cs
+++ backend/Dtos/BaseResponseDto.cs
@@ -5,8 +5,7 @@ public class BaseResponseDto<T> : BaseDto
 {
     public List<T> Data { get; private set; } = new List<T>();
     public PaginationDto Pagination { get; private set; } = new();
-
-
+    public List<string> Errors { get; private set; } = new();
 
     public BaseResponseDto(List<T> data, int statusCode = 200) : base(statusCode)
     {
@@ -18,6 +17,16 @@ public class BaseResponseDto<T> : BaseDto
     }
     public BaseResponseDto(int statusCode = 200) : base(statusCode)
     {
+    }
+
+    public BaseResponseDto(List<string> errors, int statusCode = 400) : base(statusCode)
+    {
+        Errors.AddRange(errors);
+    }
+
+    public BaseResponseDto(string error, int statusCode = 400) : base(statusCode)
+    {
+        Errors.Add(error);
     }
 
 
