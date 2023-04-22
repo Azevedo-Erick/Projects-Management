27ccb4a36c8a1bc71995e3e347da38d30ab55ddc
Azevedo-Erick <azevedoerick762@gmail.com>
2023-04-21 23:33:50 -0300

    feat: base dos dto's de permission

---
+++ backend/Dtos/Permission/CreatePermissionDto.cs
@@ -0,0 +1,13 @@
+namespace ProjectsManagement.Dtos.Permission;
+
+public class CreatePermissionDto
+{
+    public string Name { get; set; }
+    public bool Has { get; set; }
+    public CreatePermissionDto()
+    {
+        // constructor logic here
+    }
+
+    // class members here
+}
---
+++ backend/Dtos/Permission/ResponsePermissionDto.cs
@@ -0,0 +1,12 @@
+namespace ProjectsManagement.Dtos.Permission;
+
+public class ResponsePermissionDto
+{
+
+    public string Name { get; set; }
+    public bool Has { get; set; }
+    public ResponsePermissionDto()
+    {
+    }
+
+}
